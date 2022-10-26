48ea556062591cbf04211faecdeb85e75d833376
Ashmeet Singh <ashmeet@curioventures.com>
2022-10-26 23:22:16 +0530

    Gain Node updates

--- src/js/01-play-mp3-stream.js
+++ src/js/01-play-mp3-stream.js
@@ -4,21 +4,21 @@ let audio;
 function mousePressed() {
   if (!audioContext) {
     // setup our audio
-    audioContext = new AudioContext();
+    audioContext=new AudioContext();
 
     // create new <audio> tag
-    audio = document.createElement("audio");
+    audio=document.createElement("audio");
 
     // optional; enable audio looping
-    audio.loop = true;
+    audio.loop=true;
 
     // set the URL of the audio asset
-    audio.src = "audio/piano.mp3";
+    audio.src="audio/piano.mp3";
 
     // trigger audio
     audio.play();
 
-    const source = audioContext.createMediaElementSource(audio);
+    const source=audioContext.createMediaElementSource(audio);
 
     // wire the source to the 'speaker'
     source.connect(audioContext.destination);
@@ -26,7 +26,7 @@ function mousePressed() {
     // stop the audio
     audio.pause();
     audioContext.close();
-    audioContext = audio = null;
+    audioContext=audio=null;
   }
 }
 
@@ -46,21 +46,21 @@ function draw() {
   noStroke();
 
   // Draw play/pause button
-  const dim = min(width, height);
+  const dim=min(width, height);
   if (audioContext) {
-    polygon(width / 2, height / 2, dim * 0.1, 4, PI / 4);
+    polygon(width/2, height/2, dim*0.1, 4, PI/4);
   } else {
-    polygon(width / 2, height / 2, dim * 0.1, 3);
+    polygon(width/2, height/2, dim*0.1, 3);
   }
 }
 
 // Draw a basic polygon, handles triangles, squares, pentagons, etc
-function polygon(x, y, radius, sides = 3, angle = 0) {
+function polygon(x, y, radius, sides=3, angle=0) {
   beginShape();
-  for (let i = 0; i < sides; i++) {
-    const a = angle + TWO_PI * (i / sides);
-    let sx = x + cos(a) * radius;
-    let sy = y + sin(a) * radius;
+  for (let i=0;i<sides;i++) {
+    const a=angle+TWO_PI*(i/sides);
+    let sx=x+cos(a)*radius;
+    let sy=y+sin(a)*radius;
     vertex(sx, sy);
   }
   endShape(CLOSE);
--- src/js/02-play-mp3-buffer.js
+++ src/js/02-play-mp3-buffer.js
@@ -10,20 +10,21 @@ function mousePressed() {
 async function loadSound() {
   // Re-use the same context if it exists
   if (!audioContext) {
-    audioContext = new AudioContext();
+    audioContext=new AudioContext();
   }
 
   // Re-use the audio buffer as a source
   if (!audioBuffer) {
     // Fetch MP3 from URL
-    const resp = await fetch("audio/chime.mp3");
+    const resp=await fetch("audio/chime.mp3");
 
     // Turn into an array buffer of raw binary data
-    const buf = await resp.arrayBuffer();
+    const buf=await resp.arrayBuffer();
 
     // Decode the entire binary MP3 into an AudioBuffer
-    audioBuffer = await audioContext.decodeAudioData(buf);
+    audioBuffer=await audioContext.decodeAudioData(buf);
   }
+
 }
 
 async function playSound() {
@@ -34,16 +35,17 @@ async function playSound() {
   await audioContext.resume();
 
   // Now create a new "Buffer Source" node for playing AudioBuffers
-  const source = audioContext.createBufferSource();
+  const source=audioContext.createBufferSource();
 
   // Connect to destination
   source.connect(audioContext.destination);
 
   // Assign the loaded buffer
-  source.buffer = audioBuffer;
+  source.buffer=audioBuffer;
 
   // Start (zero = play immediately)
   source.start(0);
+
 }
 
 function setup() {
@@ -62,21 +64,21 @@ function draw() {
   noStroke();
 
   // Draw play/pause button
-  const dim = min(width, height);
+  const dim=min(width, height);
   if (mouseIsPressed) {
-    circle(width / 2, height / 2, dim * 0.1);
+    circle(width/2, height/2, dim*0.1);
   } else {
-    polygon(width / 2, height / 2, dim * 0.1, 3);
+    polygon(width/2, height/2, dim*0.1, 3);
   }
 }
 
 // Draw a basic polygon, handles triangles, squares, pentagons, etc
-function polygon(x, y, radius, sides = 3, angle = 0) {
+function polygon(x, y, radius, sides=3, angle=0) {
   beginShape();
-  for (let i = 0; i < sides; i++) {
-    const a = angle + TWO_PI * (i / sides);
-    let sx = x + cos(a) * radius;
-    let sy = y + sin(a) * radius;
+  for (let i=0;i<sides;i++) {
+    const a=angle+TWO_PI*(i/sides);
+    let sx=x+cos(a)*radius;
+    let sy=y+sin(a)*radius;
     vertex(sx, sy);
   }
   endShape(CLOSE);
